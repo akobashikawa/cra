e41651286ad634cfe2d5beb92f7c6d45ceb20665
Rulo Kobashikawa <akobashikawa@gmail.com>
2017-04-20 19:00:47 -0500

    Add bootstrap, jquery

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+
+// https://github.com/kevgathuku/react-bootstrap-jquery/blob/master/src/index.js
+// https://github.com/ryanflorence/react-training/blob/gh-pages/lessons/05-wrapping-dom-libs.md
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+window.jQuery = $;
+require('bootstrap');
+
 import logo from './logo.svg';
 import './App.css';
+
+const Navbar = (props) => (
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="./">
+          <img src={logo} className="App-logo" alt="logo" />
+          {props.title}
+        </a>
+      </div>
+    </div>
+  </nav>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Navbar title="Welcome to React" />
+        <div className="main">
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
